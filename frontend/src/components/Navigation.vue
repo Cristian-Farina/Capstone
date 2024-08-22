0f2686743c17a50e564592356b945aa5261d3b9f
Cristian-Farina <82306885+Cristian-Farina@users.noreply.github.com>
2024-08-22 20:52:45 +1000

    All newsletter changed

--- frontend/src/components/Navigation.vue
+++ frontend/src/components/Navigation.vue
@@ -27,9 +27,9 @@
       @update:active="(state) => toggleGroup('campaigns', state)" icon="rocket-launch-outline"
       :label="$t('Newsletter')">
       <b-menu-item :to="{ name: 'campaigns' }" tag="router-link" :active="activeItem.campaigns" data-cy="all-campaigns"
-        icon="rocket-launch-outline" :label="$t('menu.allCampaigns')" />
+        icon="rocket-launch-outline" :label="$t('All Newsletters')" />
       <b-menu-item :to="{ name: 'campaign', params: { id: 'new' } }" tag="router-link" :active="activeItem.campaign"
-        data-cy="new-campaign" icon="plus" :label="$t('menu.newCampaign')" />
+        data-cy="new-campaign" icon="plus" :label="$t('New Newsletter')" />
       <b-menu-item :to="{ name: 'media' }" tag="router-link" :active="activeItem.media" data-cy="media"
         icon="image-outline" :label="$t('menu.media')" />
       <b-menu-item :to="{ name: 'templates' }" tag="router-link" :active="activeItem.templates" data-cy="templates"
